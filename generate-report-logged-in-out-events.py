077c1f4142a0c09a2785b40ac6ebcc4be159f956
r3pairmen <89855073+r3pairmen@users.noreply.github.com>
2022-07-18 13:18:23 +0530

    Update generate-report-logged-in-out-events.py

--- generate-report-logged-in-out-events.py
+++ generate-report-logged-in-out-events.py
@@ -10,7 +10,7 @@ def current_users(events):
     if event.type = "login":
       machines[event.machine].add(event.user)
     elif event.type = "logout":
-      machines[event.machine].remvoe(event.user)
+      machines[event.machine].remove(event.user)
   return machines
 
 def generate_report(machines):
@@ -18,3 +18,24 @@ def generate_report(machines):
     if len(users) > 0:
       user_list = ", ".join(users)
       print("{}: {}".format(machine, user_list))
+
+     
+#event class
+class Event:
+  def __init__(self, event_date, event_type, machine_name, user):
+    self.date = event_date
+    self.type = event_type
+    self.machine = machine_name
+    self.user = user
+    
+#
+events = [
+    Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+    Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+    Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+    Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
+    Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
+    Event('2020-01-23 11:24:35', 'logout/login', 'mailserver.local', 'chris'),
+]
+
+
